fae6b14838b55d54704a4e96b1bb4ffe0e203441
janchowaniec <janchowaniec@odrabiamy.pl>
2021-11-06 00:25:47 +0100

    fix non deterministic sets elements order

--- collections/set.go
+++ collections/set.go
@@ -5,17 +5,19 @@ type Item interface{}
 
 // JCSet the set of Items
 type JCSet struct {
-	items map[Item]bool
+	uniques map[Item]bool
+	items   []*Item
 }
 
 // Add adds a new element to the Set. Returns a pointer to the Set.
 func (s *JCSet) Add(t Item) *JCSet {
-	if s.items == nil {
-		s.items = make(map[Item]bool)
+	if s.uniques == nil {
+		s.uniques = make(map[Item]bool)
 	}
-	_, ok := s.items[t]
+	_, ok := s.uniques[t]
 	if !ok {
-		s.items[t] = true
+		s.uniques[t] = true
+		s.items = append(s.items, &t)
 	}
 	return s
 }
@@ -23,40 +25,36 @@ func (s *JCSet) Add(t Item) *JCSet {
 // Pop - mimics Pop method from Python Set implementation ;)
 func (s *JCSet) Pop() Item {
 	var i Item
-	l := len(s.items) - 1
-	indx := 0
+	// l := len(s.items) - 1
+	// indx := 0
 	if s.emptyOrUnInit() {
 		return nil
 	}
 
-	// maps in GO behave like LIFO
-	// so in order to make Pop() behaves like in Python we need take out last element
-	// revers order of poping elements
-	for k := range s.items {
-		if indx == l {
-			i = k
-		}
-		indx++
-	}
-
-	delete(s.items, i)
+	i = *s.items[0]
+	s.removeIndex(0)
+	delete(s.uniques, i)
 
 	return i
 }
 
-// Remove - delete element from set 
+// Remove - delete element from set
 func (s *JCSet) Remove(i Item) Item {
 	if s.emptyOrUnInit() {
 		return nil
 	}
 
-	item := s.items[i]
-	delete(s.items, i)
+	item := s.uniques[i]
+	delete(s.uniques, i)
 
 	return item
 }
 
 // check if set is initialized or is not empty
 func (s *JCSet) emptyOrUnInit() bool {
-	return s.items == nil || len(s.items) == 0
+	return s.uniques == nil || len(s.uniques) == 0
+}
+
+func (s *JCSet) removeIndex(index int) {
+	s.items = append(s.items[:index], s.items[index+1:]...)
 }
